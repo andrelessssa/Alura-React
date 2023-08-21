a50a614324b03dc6a4a7fbefc6ad841c62f86793
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-08-21 16:26:45 -0300

    code
    code

--- organo/src/componetes/CampoTexto/index.js
+++ organo/src/componetes/CampoTexto/index.js
@@ -1,12 +1,16 @@
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+
+
+  
+
     return(
         <div className="campo-texto" >
             <label>
                 {props.label}
             </label>
-            <input required={props.obrigatorio} placeholder = {props.placeholder}/>
+            <input  required={props.obrigatorio} placeholder = {props.placeholder}/>
         </div>
     )
 
